0370cc66751e3ee03f99e3492c6f4ec212df9b7a
olaide-hok <olaidekareem43@yahoo.com>
2022-09-20 10:19:17 +0100

    Setup Initial Fetch Request

--- src/context.js
+++ src/context.js
@@ -4,7 +4,45 @@ export const API_ENDPOINT = `https://www.omdbapi.com/?apikey=${process.env.REACT
 const AppContext = React.createContext()
 
 const AppProvider = ({ children }) => {
-  return <AppContext.Provider value='hello'>{children}</AppContext.Provider>
+  const [isLoading, setIsLoading] = useState(true)
+  const [error, setError] = useState({ show: false, msg: ''})
+  const [movies, setMovies] = useState([])
+  const [query, setQuery] = useState('batman')
+
+  const fetchMovies = async(url) => {
+    setIsLoading(true)
+    try {
+      const response = await fetch(url)
+      const data = await response.json()
+
+      if (data.Response === 'True') {
+        setMovies(data.Search)
+        setError({ show: false, msg: ''})
+        
+      } else {
+        setError({ show: false, msg: '' })
+      }
+      setIsLoading(false)
+
+    } catch (error) {
+      setIsLoading(false)      
+    }
+  }
+
+  useEffect(() => {
+    fetchMovies(`${API_ENDPOINT}&s=${query}`)
+  }, [query])
+
+
+
+  return <AppContext.Provider value={{
+    isLoading,
+    error,
+    movies,
+    query,
+    setQuery
+
+  }}>{children}</AppContext.Provider>
 }
 // make sure use
 export const useGlobalContext = () => {
